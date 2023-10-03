ccd571f1fd712a0a28b25719f64871d30dc70299
WebCasting28 <ansarisikkim786@gmail.com>
2023-10-03 08:30:14 -0700

    Skullcandy

---
+++ app.js
@@ -0,0 +1,127 @@
+//first  sub-menu js
+
+let button1 = document.getElementById("btn1");
+
+let box1 = document.getElementById("sub-menu1");
+
+let show1 = document.getElementById("sub-show1");
+
+button1.addEventListener('mouseenter', ()=>{
+
+    
+   box1.style.top="60px";
+    
+})
+
+
+button1.addEventListener('mouseleave', ()=>{
+    box1.style.top="-100%";
+
+
+}) 
+show1.addEventListener('mouseenter', ()=>{
+    box1.style.top="60px";
+
+
+})
+show1.addEventListener('mouseleave', ()=>{
+    box1.style.top="-100%";
+
+
+})
+
+//second  sub-menu js
+
+let button2 = document.getElementById("btn2");
+
+let box2 = document.getElementById("sub-menu2");
+
+let show2 = document.getElementById("sub-show2");
+
+button2.addEventListener('mouseenter', ()=>{
+
+    
+   box2.style.top="60px";
+    
+})
+
+
+button2.addEventListener('mouseleave', ()=>{
+    box2.style.top="-100%";
+
+
+}) 
+show2.addEventListener('mouseenter', ()=>{
+    box2.style.top="60px";
+
+
+})
+show2.addEventListener('mouseleave', ()=>{
+    box2.style.top="-100%";
+
+
+})
+
+//third  sub-menu js
+
+let button3 = document.getElementById("btn3");
+
+let box3 = document.getElementById("sub-menu3");
+let show3 = document.getElementById("sub-show3");
+
+button3.addEventListener('mouseenter', ()=>{
+
+    
+   box3.style.top="60px";
+    
+})
+
+
+button3.addEventListener('mouseleave', ()=>{
+    box3.style.top="-100%";
+
+
+}) 
+
+show3.addEventListener('mouseenter', ()=>{
+    box3.style.top="60px";
+
+
+})
+show3.addEventListener('mouseleave', ()=>{
+    box3.style.top="-100%";
+
+
+}) 
+
+//fourth  sub-menu js
+
+let button4 = document.getElementById("btn4");
+
+let box4 = document.getElementById("sub-menu4");
+let show4 = document.getElementById("sub-show4");
+
+button4.addEventListener('mouseenter', ()=>{
+
+    
+   box4.style.top="60px";
+    
+})
+
+
+button4.addEventListener('mouseleave', ()=>{
+    box4.style.top="-100%";
+
+
+}) 
+
+show4.addEventListener('mouseenter', ()=>{
+    box4.style.top="60px";
+
+
+})
+show4.addEventListener('mouseleave', ()=>{
+    box4.style.top="-100%";
+
+
+}) 
